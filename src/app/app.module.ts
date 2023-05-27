6877291c7b4a47d509e08130c08f363ca60e4859
Dmitry Samsonov <dmitry.samsonov@ecentria.com>
2023-05-27 17:33:53 -0500

    First draft with ingredient list and edit components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { IngredientListComponent } from './ingredients/ingredient-list/ingredient-list.component';
+import { IngredientEditComponent } from './ingredients/ingredient-edit/ingredient-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IngredientListComponent,
+    IngredientEditComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/ingredients/ingredient-list/ingredient-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Ingredient } from "../ingredient.model";
+
+@Component({
+  selector: 'app-ingredient-list',
+  templateUrl: './ingredient-list.component.html',
+  styleUrls: ['./ingredient-list.component.css']
+})
+export class IngredientListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5, 'ea'),
+    new Ingredient('Tomatoes', 10, 'ea'),
+  ];
+}
